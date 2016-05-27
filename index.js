d74d165a7b217a07c85e81586ce6d353d393e53c
Ryan Cunningham <rcunning@gmail.com>
2016-05-27 15:00:14 -0700

    get event param padded to app, read and return stdout

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ var invokeRubyApp = "./app";
 
 exports.handler = function(event, context) {
   console.log("Starting process: " + invokeRubyApp);
-  var child = spawn(invokeRubyApp, [ JSON.stringify(event), "TEST" ]);
+  var child = spawn(invokeRubyApp, [ JSON.stringify(event) ]); //, { stdio: 'inherit' });
+  var out_data = '';
 
-  child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
-  child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
+  child.stdout.on('data', function(data) { out_data += data; });
+  child.stderr.on('data', function(data) { console.log("stderr:\n" + data); });
 
-  child.on('close', function (code) {
+  child.on('close', function(code) {
     if(code === 0) {
-      context.succeed("Process completed: " + invokeRubyApp);
+      context.succeed(out_data);
     } else {
-      context.fail("Process \"" + invokeRubyApp + "\" exited with code: " + code);
+      context.fail(new Error("Process exited with non-zero status code " + code));
     }
   });
 }
